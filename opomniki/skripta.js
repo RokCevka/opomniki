b6812ee1b414e0a9e018f08b648d20e6e129ed43
Rok Čevka <rc0491@student.uni-lj.si>
2016-03-08 18:23:48 +0000

    strings

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -9,7 +9,15 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
-	
+			
+			if(cas > 0){
+				casovnik.innerHTML=cas-1;
+			}
+			else{
+				var naziv = document.querySelector(".naziv_opomnika").innerHTML;
+				//throw alert
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
@@ -19,14 +27,29 @@ window.addEventListener('load', function() {
 	
 	var prijavi=function(event){
 		var uporabnik = document.querySelector("#uporabnisko_ime").value;	
-	
+	document.querySelector("#uporabnik").innerHTML = uporabnik;
 	document.querySelector(".pokrivalo").style.visibility="none";
 	document.querySelector(".pokrivalo").style.display="none";
 	
-	document.querySelector("#uporabnik").innerHTML = uporabnik;
+	
 	}
 
 	document.querySelector("#prijavniGumb").addEventListener('click', prijavi);
+
+	
+	var dodajOpomnik=function(event){
+		var naziv = document.querySelector("#naziv_opomnika").value;
+		var dolzina = document.querySelector("#cas_opomnika").value;
+		
+		document.querySelector("#naziv_opomnika").value="";
+		document.querySelector("#cas_opomnika").value="";
+		
+		
+	document.querySelector("#opomniki").innerHTML+="<div class='opomnik'>"
+            +"<div class='naziv_opomnika'>"+naziv+"</div>"
+            +"<div class='cas_opomnika'> Opomnik čez <span>"+dolzina+"</span> sekund.</div></div>";
+	}
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 
 	
 });
